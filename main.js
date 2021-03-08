0abc3e74ab9e78163a2024e789ce28e00422520a
Ryan Maloney <rypmaloney@gmail.com>
2021-03-08 15:40:17 -0500

    Finalized layout, added documents and images

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+
+//scrollspy
+$('body').scrollspy({ target: '#navbar-example2' })
